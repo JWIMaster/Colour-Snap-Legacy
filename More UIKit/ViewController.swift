d07f308753f61ea279ad7a2364e376265912892d
JWI <jwalr@JWIs-MacBook-Pro-2.local>
2025-08-28 16:15:25 +1000

    Initial Commit

---
+++ More UIKit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  More UIKit
+//
+//  Created by JWI on 28/08/2025.
+//  Copyright (c) 2025 JWI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
